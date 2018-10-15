b3ad1db1836da0d2d6bbd0cd640b444147107406
maulana saputra <maulanasaputra@gmail.com>
2018-10-15 14:40:03 +0700

    master pusing .. cuma buat ngetes memory doang
    Signed-off-by: maulana saputra <maulanasaputra@gmail.com>

--- module/Album/test/Controller/AlbumControllerTest.php
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -5,9 +5,16 @@ use Album\Controller\AlbumController;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
+use Album\Model\AlbumTable;
+use Zend\ServiceManager\ServiceManager;
+
+use Album\Model\Album;
+use Prophecy\Argument;
+
 class AlbumControllerTest extends AbstractHttpControllerTestCase
 {
 	protected $traceError = true;
+	protected $albumTable;
 	
 	public function setUp()
 	{
@@ -17,6 +24,8 @@ class AlbumControllerTest extends AbstractHttpControllerTestCase
 			$configOverrides
 		));
 		parent::setUp();
+		
+		$this->configureServiceManager($this->getApplicationServiceLocator());
 		
 		$services = $this->getApplicationServiceLocator();
 		$config = $services->get('config');
@@ -28,11 +37,54 @@ class AlbumControllerTest extends AbstractHttpControllerTestCase
 	
 	public function testindexActionCanBeAccessed()
 	{
+		$this->albumTable->fetchAll()->willReturn([]);
+		
 		$this->dispatch('/album');
 		$this->assertResponseStatusCode(200);
 		$this->assertModuleName('Album');
 		$this->assertControllerName(AlbumController::class);
 		$this->assertControllerClass('AlbumController');
 		$this->assertMatchedRouteName('album');
+	}
+	
+	public function testAddActionRedirectsAfterValidPost()
+	{
+		$this->albumTable
+			->saveAlbum(Argument::type(Album::class))
+			->shouldBeCalled();
+			
+		$postData = [
+			'title' => 'Led Zeppelin III',
+			'artist' => 'Led Zeppelin',
+			'id' => '',
+		];
+		
+		$this->dispatch('/album/add', 'POST', $postData);
+		$this->assertResponseStatusCode(302);
+		$this->assertRedirectTo('/album');
+		
+		$this->albumTable->getAlbum(3)->willReturn(new Album());
+	}
+	
+	protected function configureServiceManager(ServiceManager $services)
+	{
+		$services->setAllowOverride(true);
+		
+		$services->setService('config', $this->updateConfig($services->get('config')));
+		$services->setService(AlbumTable::class, $this->mockAlbumTable()->reveal());
+		
+		$services->setAllowOverride(false);
+	}
+	
+	protected function updateConfig($config)
+	{
+		$config['db'] = [];
+		return $config;
+	}
+	
+	protected function mockAlbumTable()
+	{
+		$this->albumTable = $this->prophesize(AlbumTable::class);
+		return $this->albumTable;
 	}
 }
