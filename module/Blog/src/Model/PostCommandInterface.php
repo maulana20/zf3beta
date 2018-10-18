ac35be7b3332197ab466945d64060f96b862e3c6
maulana saputra <maulanasaputra@gmail.com>
2018-10-18 11:40:41 +0700

    master delete blog
    Signed-off-by: maulana saputra <maulanasaputra@gmail.com>

--- module/Blog/src/Model/PostCommandInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Blog\Model;
-
-interface PostCommandInterface
-{
-	public function insertPost(Post $post);
-	public function updatePost(Post $post);
-	public function deletePost(Post $post);
-}
