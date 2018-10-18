7f679f0800a5b477d60562b833fd6b80b589c246
maulana saputra <maulanasaputra@gmail.com>
2018-10-18 10:30:27 +0700

    master di buat simple routing di dalam modul semuaaaaaaaaa
    Signed-off-by: maulana saputra <maulanasaputra@gmail.com>

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -15,6 +15,11 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
 //use Zend\ModuleManager\ModuleManager;
 //use Zend\Mvc\MvcEvent;
 
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+use Zend\Navigation;
+
 class Module implements ConfigProviderInterface
 {
 	const VERSION = '3.0.3-dev';
@@ -40,7 +45,96 @@ class Module implements ConfigProviderInterface
 	
 	public function getConfig()
 	{
-		return include __DIR__ . '../config/module.config.php';
+		return [
+			'router' => [
+				'routes' => [
+					'home' => [
+						'type' => Literal::class,
+						'options' => [
+							'route'    => '/',
+							'defaults' => [
+								'controller' => Controller\IndexController::class,
+								'action'     => 'index',
+							],
+						],
+					],
+					'application' => [
+						'type'    => Segment::class,
+						'options' => [
+							'route'    => '/application[/:action]',
+							'defaults' => [
+								'controller' => Controller\IndexController::class,
+								'action'     => 'index',
+							],
+						],
+					],
+					'album' => [
+						'type'    => Segment::class,
+						'options' => [
+							'route' => '/album[/:action[/:id]]',
+							'constraints' => [
+								'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+								'id'     => '[0-9]+',
+							],
+							'defaults' => [
+								'controller' => Controller\AlbumController::class,
+								'action'     => 'index',
+							],
+						],
+					],
+				],
+			],
+			'controllers' => [
+				'factories' => [
+					Controller\IndexController::class => InvokableFactory::class
+				],
+			],
+			'view_manager' => [
+				'display_not_found_reason' => true,
+				'display_exceptions'       => true,
+				'doctype'                  => 'HTML5',
+				'not_found_template'       => 'error/404',
+				'exception_template'       => 'error/index',
+				'template_map' => [
+					'layout/layout'           => dirname(__DIR__) . DIRECTORY_SEPARATOR . __NAMESPACE__ . '/view/layout/layout.phtml',
+					'application/index/index' => dirname(__DIR__) . DIRECTORY_SEPARATOR . __NAMESPACE__ . '/view/application/index/index.phtml',
+					'error/404'               => dirname(__DIR__) . DIRECTORY_SEPARATOR . __NAMESPACE__ . '/view/error/404.phtml',
+					'error/index'             => dirname(__DIR__) . DIRECTORY_SEPARATOR . __NAMESPACE__ . '/view/error/index.phtml',
+				],
+				'template_path_stack' => [
+					dirname(__DIR__) . DIRECTORY_SEPARATOR . __NAMESPACE__ . '/view',
+				],
+			],
+			'navigation' => [
+				'default' => [
+					[
+						'label' => 'Home',
+						'route' => 'home',
+					],
+					[
+						'label' => 'Album',
+						'route' => 'album',
+						'pages' => [
+							[
+								'label' => 'Add',
+								'route' => 'album',
+								'action' => 'add',
+							],
+							[
+								'label' => 'Edit',
+								'route' => 'album',
+								'action' => 'edit',
+							],
+							[
+								'label' => 'Delete',
+								'route' => 'album',
+								'action' => 'delete',
+							],
+						],
+					],
+				],
+			],
+		];
 	}
 	
 	public function getServiceConfig()
